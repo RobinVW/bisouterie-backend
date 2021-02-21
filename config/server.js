d0f00e16e9221941053ac79b04d98ba1fd895e0c
RobinVW <robin.vanwelden@gmail.com>
2021-02-22 00:25:22 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6eb3d7712b267615ab8df3c5731895cf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '63d3fc71-1b17-4303-b382-20547e23a777'
+};
