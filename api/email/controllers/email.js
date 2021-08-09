dfbbd5cc1a209114374f07aeaf8242dde58442b3
RobinVW <robin.vanwelden@gmail.com>
2021-08-09 19:37:39 +0200

    Toevoegen /mails post api om berichten te sturen via frontend

---
+++ api/email/controllers/email.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/**
+ * Read the documentation () to implement custom controller functions
+ */
+
+module.exports = {
+  /**
+   * Sends an email to the recipient in the body of the request
+   */
+  send: async (ctx) => {
+    const body = ctx.request.body
+    const from = body.email
+    strapi.log.debug(`Trying to receive a message from ${from}`)
+
+    try {
+      const emailOptions = {
+        from: 'bisouterie.amelie@gmail.com',
+        to: 'bisouterie.amelie@gmail.com',
+        subject: `Message From ${body.firstName} ${body.lastName}`,
+        text: body.message +' | Sent from: ' +body.email,
+        html: `<div>${body.message}</div><p>Sent from: ${body.email}</p>`
+      }
+      await strapi.plugins['email'].services.email.send(emailOptions)
+      strapi.log.debug(`Email sent to ${sendTo}`)
+      ctx.send({ message: 'Email sent' })
+    } catch (err) {
+      strapi.log.error(`Error sending email to bisouterie.amelie@gmail.com`, err)
+    //   ctx.send({ error: 'Error sending email' })
+      ctx.send(err);
+    }
+  },
+}
