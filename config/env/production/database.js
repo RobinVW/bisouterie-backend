293bcd9508a1b729ec3adb057d2622cc5b7eb193
Robin Van Welden <robin.vanwelden@student.hogent.be>
2023-01-21 16:41:51 +0100

    changes in package.json

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,39 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: config.host,
+//         port: config.port,
+//         database: config.database,
+//         username: config.user,
+//         password: config.password,
+//         ssl: {
+//           rejectUnauthorized: false
+//         }
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false
-        }
-      },
-      options: {
-        ssl: true,
-      },
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5931),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'password'),
+      ssl: env.bool(true),
     },
   },
 });
