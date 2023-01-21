1d9ed3c4a6a7b419c634a835a66b32bbe7a6f047
Robin Van Welden <robin.vanwelden@student.hogent.be>
2023-01-21 22:18:57 +0100

    db config change

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -24,15 +24,29 @@
 //   },
 // });
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('PGHOST', '127.0.0.1'),
+//       port: env.int('PGPORT', 5931),
+//       database: env('PGDATABASE', 'railway'),
+//       user: env('PGUSER', 'postgres'),
+//       password: env('PGPASSWORD', 'password'),
+//       ssl: env.bool(true),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5931),
-      database: env('PGDATABASE', 'railway'),
-      user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'password'),
+      host: env('PGHOST'),
+      port: env.int('PGPORT'),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
       ssl: env.bool(true),
     },
   },
